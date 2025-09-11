d0b2eb5fe227bdb703b9cb971e7bdb3235500a8e
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-09-11 09:06:17 -0300

    Aula 2 - 03 Escopo de variável - att

--- escopo.js
+++ escopo.js
@@ -8,13 +8,15 @@ if (1 > 0) {
 estudante = 'Ana';
 console.log(estudante)
 
-// Com let é possível fazer o mesmo
+// Com let parece que temos o mesmo resultado que var, mas agora na verdade possuimos duas variáveis professor, uma dentro do if e outra fora.
 if (2 < 3) {
     let professor = 'Jacob';
     console.log(professor);
 }
 
-professor = 'Jorge'
+// Nesse caso que a variável foi declarada sem especificar se a mesma é const, let ou var a mesma vai automaticamente ser var.
+professor = 'Jorge';
+// Caso comentemos a variável professor a cima o código irá quebrar, pois não temos acesso a variável professor dentro do escopo do if
 console.log(professor);
 
 // É possível alterar o valor de uma variáel criada fora do escopo
