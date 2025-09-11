b15f462a3649289979ec06f654bf4c7d124dd2d3
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-09-11 11:02:22 -0300

    Aula 2 - 06 Lidando com erros

---
+++ erros.js
@@ -0,0 +1,5 @@
+// console.log() com uma variável que não foi decalarada, o que irá gerar um erro
+// console.log(variavel);
+
+// Erro de sintax no console.log(), pois o mesmo falta o ")"
+// console.log('oi';
