32fe0935c22fd7b7db16d4cf04077d4bc3b636a3
GuilFran Aspire7 <guilfranca27@gmail.com>
2025-09-12 11:32:06 -0300

    Aula 3 - 08 Boolean

---
+++ Aula 3/tipo_boolean.js
@@ -0,0 +1,20 @@
+// Tipo boolean possui apenas dois valores -> true e false
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+// if (estaAprovado === true) {
+if (estaAprovado) {
+    console.log('Parabéns, boas festas!');
+} else {
+    console.log('Reprovado kkkkkkkkkkkk');
+}
+
+// if (estaAprovado === false) {
+if (!estaAprovado) {
+    console.log('Reprovado kkkkkkkkkkkk');
+} else {
+    console.log('Parabéns, boas festas!');
+}
+
+// Tipo boolean é o resultado de qualquer comparação, sendo ela true ou false.
+// O if é ativado toda vez que o resultado esperado for verdadeiro.
