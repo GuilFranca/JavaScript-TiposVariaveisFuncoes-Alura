7780dc3b04cbc9eb8d84fb7cb1a781c009522647
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-09-12 15:56:38 -0300

    Aula 3 - 10 null e undefined - Finalizado

--- Aula 3/tipo_null_undefine.js
+++ Aula 3/tipo_null_undefine.js
@@ -1,3 +1,12 @@
+// undefine -> Não definido
 let nomeEstudante;
 console.log(nomeEstudante);
-console.log(typeof nomeEstudante);
+console.log(typeof nomeEstudante); // Tipo da variável
+
+// null -> Vázio
+let telefoneEstudante = null;
+console.log(typeof telefoneEstudante); // Object -> erro
+console.log(telefoneEstudante + 1); // null + 1 = 1
+console.log(nomeEstudante + 1); // undefine + 1 = NaN -> tipo number
+
+const professor = 'Ana';
