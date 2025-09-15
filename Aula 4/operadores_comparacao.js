a371f7d811fe7a952b6a1029e8c3b31db7c220f9
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-09-15 13:26:00 -0300

    Aula 4 - 10 Operadores de comparação

---
+++ Aula 4/operadores_comparacao.js
@@ -0,0 +1,21 @@
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+    console.log('Aprovado');
+}
+
+// Compara o conteúdo do valor ==
+if ('0' == 0) {
+    console.log("'0' = 0");
+} else {
+    console.log("'0' != 0");
+}
+
+// Compara o valor e o tipo de dado ===
+if ('0' === 0) {
+    console.log("'0' = 0");
+} else {
+    console.log("'0' != 0");
+}
+
+// Preferência pela utilização do ===
